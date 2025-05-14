16f48441d80efba523b76b4373e66ad4b32d238d
dev-imani <michaelimani396@gmail.com>
2025-05-14 12:22:11 +0300

    myThirdCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import Hero from "./Components/Hero"
 import Navbar from "./Components/Navbar"
+import Services from "./Components/Services"
 
 
 const App = () => {
@@ -7,6 +9,8 @@ const App = () => {
     {  /* hy */}
       <main className="overflow-x-hidden antialiased text-neutral-800"></main>
       <Navbar/>
+      <Hero/>
+      <Services/>
       </>
         
   )
